c8b496149a1fcdd6c8b0e609f6f5f4972bd02854
11111Asif <asif040502@gmail.com>
2025-08-16 10:04:07 -0700

    first commit

---
+++ even.js
@@ -0,0 +1,7 @@
+let numbers = 44;
+if(numbers % 2 == 0){
+    console.log('this is a even number')
+}
+else{
+    console.log('this is a odd number')
+}
---
+++ odd.js
@@ -0,0 +1,7 @@
+let numbers = 99;
+if(numbers % 2 !== 1){
+    console.log('this is a even number')
+}
+else{
+    console.log('this is a odd number')
+}
